38af571559328547daad2b933618288d074e6763
mori07 <extend71@gmail.com>
2016-08-06 09:12:38 +0000

    add quick sort

--- lib/group_d_2016_gem.rb
+++ lib/group_d_2016_gem.rb
@@ -1,5 +1,28 @@
 require "group_d_2016_gem/version"
 
 module GroupD2016Gem
-  # Your code goes here...
+    class Sorter
+    def bubble_sort(a)
+        n = a.size
+        0.upto(n - 2) do |i|
+            (n - 1).downto(i + 1) do |j|
+                if a[j] < a[j - 1]
+                    a[j], a[j - 1] = a[j - 1], a[j]
+                    p a
+                end
+            end
+        end 
+    end
+
+    def quick_sort(a)
+        if a.size <= 1
+            return a
+        end
+        pivot = a.delete_at(a.size / 2)
+        smaller, bigger = a.partition {|i| i < pivot}
+        ret = quick_sort(smaller) + [pivot] + quick_sort(bigger)
+        return ret  
+    end
 end
+end
+
--- test/group_d_2016_gem_test.rb
+++ test/group_d_2016_gem_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class GroupD2016GemTest < Minitest::Test
   def setup
-      @sorter = ::GroupD2016GemTest::Sorter.new
+      @sorter = ::GroupD2016Gem::Sorter.new
   end
   def test_that_it_has_a_version_number
     refute_nil ::GroupD2016Gem::VERSION
@@ -12,9 +12,8 @@ class GroupD2016GemTest < Minitest::Test
     assert_equal([-21, -3, 0, 1, 4, 10, 10000], 
 	@sorter.merge_sort(([0, -3, 10, 1, 4, 10000, -21])), 'merge sort'
   end
-
+  
   def test_quick_sort
-    assert_equal([-21, -3, 0, 1, 4, 10, 10000], 
-	@sorter.quick_sort(([0, -3, 10, 1, 4, 10000, -21])), 'quick sort'
+    assert_equal([-21, -3, 0, 1, 4, 10, 10000],@sorter.quick_sort([0, -3, 10, 1, 4, 10000, -21]), 'quick sort')
   end
 end
