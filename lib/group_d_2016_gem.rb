46808bc651318f488a8873511e4aa9da816d0e9a
None <a1601hi@aiit.ac.jp>
2016-08-06 08:16:59 +0000

    select sort2

--- lib/group_d_2016_gem.rb
+++ lib/group_d_2016_gem.rb
@@ -1,5 +1,15 @@
 require "group_d_2016_gem/version"
 
 module GroupD2016Gem
-  # Your code goes here...
-end
+class Sorter
+  def select_sort
+    tmp = self.dup
+    res = []
+    res.push tmp.delete_min until tmp.empty?
+    res
+  end
+  def delete_min
+    min_idx = find_index { |item| item == self.min }
+    delete_at(min_idx)
+  end
+end 
